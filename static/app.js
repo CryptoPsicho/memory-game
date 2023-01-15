4a72830202dbe3dfb63ac168c99ebcaad3fee0eb
CryptoPsicho <77224223+CryptoPsicho@users.noreply.github.com>
2023-01-15 12:26:56 +0100

    gh

--- static/app.js
+++ static/app.js
@@ -3,83 +3,83 @@ document.addEventListener('DOMContentLoaded', () => {
   const cardArray = [
     {
       name: 'batman',
-      img: './heroes/batman.png'
+      img: '/heroes/batman.png'
     },
     {
       name: 'captain_america',
-      img: './heroes/captain_america.png'
+      img: '/heroes/captain_america.png'
     },  
     {
       name: 'flash',
-      img: './heroes/flash.png'
+      img: '/heroes/flash.png'
     }, 
     {
       name: 'girl_spider',
-      img: './heroes/girl_spider.png'
+      img: '/heroes/girl_spider.png'
     }, 
     {
       name: 'hulk',
-      img: './heroes/hulk.png'
+      img: '/heroes/hulk.png'
     }, 
     {
       name: 'iron_man',
-      img: './heroes/iron_man.png'
+      img: '/heroes/iron_man.png'
     }, 
     {
       name: 'spider_man',
-      img: './heroes/spider_man.png'
+      img: '/heroes/spider_man.png'
     }, 
     {
       name: 'superman',
-      img: './heroes/superman.png'
+      img: '/heroes/superman.png'
     }, 
     {
       name: 'thor',
-      img: './heroes/thor.png'
+      img: '/heroes/thor.png'
     }, 
     {
       name: 'wonder_woman',
-      img: './heroes/wonder_woman.png'
+      img: '/heroes/wonder_woman.png'
     }, 
     {
       name: 'batman',
-      img: './heroes/batman.png'
+      img: '/heroes/batman.png'
     },
     {
       name: 'captain_america',
-      img: './heroes/captain_america.png'
+      img: '/heroes/captain_america.png'
     },  
     {
       name: 'flash',
-      img: './heroes/flash.png'
+      img: '/heroes/flash.png'
     }, 
     {
       name: 'girl_spider',
-      img: './heroes/girl_spider.png'
+      img: '/heroes/girl_spider.png'
     }, 
     {
       name: 'hulk',
-      img: './heroes/hulk.png'
+      img: '/heroes/hulk.png'
     }, 
     {
       name: 'iron_man',
-      img: './heroes/iron_man.png'
+      img: '/heroes/iron_man.png'
     }, 
     {
       name: 'spider_man',
-      img: './heroes/spider_man.png'
+      img: '/heroes/spider_man.png'
     }, 
     {
       name: 'superman',
-      img: './heroes/superman.png'
+      img: '/heroes/superman.png'
     }, 
     {
       name: 'thor',
-      img: './heroes/thor.png'
+      img: '/heroes/thor.png'
     }, 
     {
       name: 'wonder_woman',
-      img: './heroes/wonder_woman.png'
+      img: '/heroes/wonder_woman.png'
     }, 
 
   ]
@@ -96,7 +96,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
       const card = document.createElement('img')
-      card.setAttribute('src', './heroes/back.png')
+      card.setAttribute('src', '/heroes/back.png')
       card.setAttribute('data-id', i)
       card.addEventListener('click', flipCard)
       grid.appendChild(card)
@@ -110,20 +110,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const optionTwoId = cardsChosenId[1]
     
     if(optionOneId == optionTwoId) {
-      cards[optionOneId].setAttribute('src', './heroes/back.png')
-      cards[optionTwoId].setAttribute('src', './heroes/back.png')
+      cards[optionOneId].setAttribute('src', '/heroes/back.png')
+      cards[optionTwoId].setAttribute('src', '/heroes/back.png')
       alert('You have clicked the same image!')
     }
     else if (cardsChosen[0] === cardsChosen[1]) {
       alert('You found a match')
-      cards[optionOneId].setAttribute('src', './heroes/match.gif')
-      cards[optionTwoId].setAttribute('src', './heroes/match.gif')
+      cards[optionOneId].setAttribute('src', '/heroes/match.gif')
+      cards[optionTwoId].setAttribute('src', '/heroes/match.gif')
       cards[optionOneId].removeEventListener('click', flipCard)
       cards[optionTwoId].removeEventListener('click', flipCard)
       cardsWon.push(cardsChosen)
     } else {
-      cards[optionOneId].setAttribute('src', './heroes/back.png')
-      cards[optionTwoId].setAttribute('src', './heroes/back.png')
+      cards[optionOneId].setAttribute('src', '/heroes/back.png')
+      cards[optionTwoId].setAttribute('src', '/heroes/back.png')
       alert('Sorry, try again')
     }
     cardsChosen = []
