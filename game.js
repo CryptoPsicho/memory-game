01d491a09c6734d4ee229203d7c9b673097c1650
CryptoPsicho <77224223+CryptoPsicho@users.noreply.github.com>
2023-01-15 12:07:16 +0100

    Port forwarding

--- game.js
+++ game.js
@@ -1,19 +1,20 @@
-const express = require('express')
-const { appendFile } = require('fs')
-const path = require('path')
+const express = require('express');
+const { appendFile } = require('fs');
+const path = require('path');
+const port = 3000;
 
-const game = express()
+const game = express();
 
-game.use(express.static('/static'))
+game.use(express.static('/static'));
 
 game.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, './static/index.html'))
-})
+});
 
 game.all('*', (req,res) => {
     res.status(404).send('Url does not exist')
-})
+});
 
-game.listen(443, () => {
-    console.log('Ciao from 443')
-})
+game.listen(process.env.PORT || port, () => {
+    console.log('Ciao from http://localhost:${port}')
+});
